95c1d06ad50ebdf6a8f4214d21c846c503f75562
Bhargav Killada <bhargavan@iisc.ac.in>
2023-05-29 09:47:38 +0530

    main.py and web

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+from flask import Flask, request, render_template, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
+#from src.pipeline.training_pipeline import
 
 
+application = Flask(__name__)
+app = application
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['GET','POST'])
+
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        data = CustomData(
+            carat = float(request.form.get('carat')),
+            depth=float(request.form.get('depth')),
+            table=float(request.form.get('table')),
+            x=float(request.form.get('x')),
+            y=float(request.form.get('y')),
+            z=float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color = request.form.get('color'),
+            clarity=request.form.get('clarity')
+        )
+        final_new_data = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(final_new_data)
+
+        results =round(pred[0],2)
+
+        return render_template('result.html', final_result = results)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(host ='0.0.0.0', debug = True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,11 @@ def model_eval(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         logging.info('Exception occured in Model_Eval')
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object')
+        raise CustomException(e, sys)
